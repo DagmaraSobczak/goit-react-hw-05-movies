7b0054b0ad78aef8fbc777be5c30b7683ef18a5b
DagmaraSobczak <daga.sobczak@gmail.com>
2023-06-26 20:28:15 +0200

    added structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
+import React from 'react';
+import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const SharedLayout = lazy(() => import('./Shared Layout/Shared Layout'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+const Home = lazy(() => import('../pages/Home/Home'));
+const Movie Details = lazy(() => import('../pages/Movie Details/Movie Details'));
+const Movies = lazy(() => import('../pages/Movies/Movies'));
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route path="some-path" element={<MyComponent />} />
+        {/* Inne trasy */}
+      </Route>
+    </Routes>
   );
 };
---
+++ src/components/Shared Layout/Shared Layout.jsx
@@ -0,0 +1,17 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+
+const SharedLayout = () => {
+  return (
+    <Container>
+      <AppBar>
+        <Navigation />
+        <UserMenu />
+      </AppBar>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
+    </Container>
+  );
+};
